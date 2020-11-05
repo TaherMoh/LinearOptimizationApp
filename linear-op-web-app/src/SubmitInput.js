5ee7333e4210cc193560aeab1502c52d07785bc5
taherMmohammad <taherules5@gmail.com>
2020-11-05 17:45:20 -0500

    Autocomplete text box and API work

--- linear-op-web-app/src/SubmitInput.js
+++ linear-op-web-app/src/SubmitInput.js
@@ -1,10 +1,26 @@
 import React, { Component } from "react";
 import {withStyles} from "@material-ui/core/styles";
+import { MDBInput } from "mdbreact";
+import ReactTextareaAutocomplete from "@webscopeio/react-textarea-autocomplete";
+import './App.css'
 
 const styles = theme => ({
   submitBox: {
-    width: '100%',
-    height: '100%',
+    fontSize: '20px',
+    padding: '5px',
+    border: '2px solid black',
+  },
+  
+  label: {
+    left: 0,
+  },
+
+  popup:{
+    position: 'relative',
+  },
+
+  rta:{
+    position: 'relative',
   }
 });
 
@@ -13,7 +29,8 @@ class SubmitInput extends Component {
   constructor() {
     super();
     this.state = {
-      textAreaValue: ""
+      textAreaValue: "",
+      values: [],
     };
     this.handleChange = this.handleChange.bind(this);
   }
@@ -22,23 +39,106 @@ class SubmitInput extends Component {
     this.setState({ textAreaValue: event.target.value });
   }
 
-  handleSubmit() {
-    alert('A name was submitted: ' + this.state.textAreaValue);
+  async handleSubmit() {
+    alert('Text was submitted: ' + this.state.textAreaValue);
+
+    // POST request using fetch with async/await
+    const requestOptions = {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: this.state.textAreaValue,
+    };
+
+    fetch('http://localhost:8080/test_Input', requestOptions).then(function (response) {
+        if (response.ok) {
+            return response.json();
+        } else {
+            return Promise.reject(response);
+        }
+    }).then(result => {
+        console.log(result);
+
+        this.setState({
+            ...this.state,
+            values: this.state.textAreaValue,
+        })
+    })
   }
 
   render() {
     const { classes } = this.props;
+    const Item = ({ entity: { name, char } }) => <div style={{font: '20px'}}>{`${char}`}</div>;
 
     return (
-      <div>
-          <label>Enter value : </label>
-          <textarea
-            value={this.state.textAreaValue}
-            onChange={this.handleChange}
-            className={classes.submitBox}
-          />
+      <div className="container">
+          <label className={classes.label}>Enter value : </label>
+          <div>
+            {/* <MDBInput
+              type="textarea"
+              label="Icon Prefix"
+              rows="2"
+              icon="pencil-alt"
+              value={this.state.textAreaValue}
+              onChange={this.handleChange}
+              className={classes.submitBox}
+            /> */}
 
-          <button onClick={() => this.handleSubmit()}>Submit</button>
+            <div className="container">
+              <ReactTextareaAutocomplete
+                className={classes.submitBox}
+                onChange={this.handleChange}
+                loadingComponent={() => <span>Loading</span>}
+                style={{position: 'relative', minWidth: '100%', minHeight: '300px', listStyleType: 'none',}}
+                dropdownStyle={{ position: 'absolute', listStyleType: 'none', listStyleType: 'none' }}
+                listStyle={{ listStyleType: 'none' }}
+                renderToBody={true}
+                itemStyle={{fontSize: '20px'}}
+                trigger={{
+                  "v": {
+                    dataProvider: token => {
+                      return [
+                        { name: "var: ", char: "var" },
+                      ];
+                    },
+                    component: Item,
+                    output: (item) => item.name
+                  },
+
+                  "ma": {
+                    dataProvider: token => {
+                      return [
+                        { name: "maximize: ", char: "maximize" },
+                      ];
+                    },
+                    component: Item,
+                    output: (item) => item.name
+                  },
+
+                  "su": {
+                    dataProvider: token => {
+                      return [
+                        { name: "subject to: ", char: "subject to" },
+                      ];
+                    },
+                    component: Item,
+                    output: (item) => item.name
+                  },
+
+                  "st": {
+                    dataProvider: token => {
+                      return [
+                        { name: "step: ", char: "step" },
+                      ];
+                    },
+                    component: Item,
+                    output: (item) => item.name
+                  },
+                }}
+              />
+            </div>
+
+            <button onClick={() => this.handleSubmit()}>Solve</button>
+          </div>
       </div>
     );
   }
